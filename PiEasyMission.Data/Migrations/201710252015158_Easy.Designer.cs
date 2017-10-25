8d895a7760b0592af6e06420e4c316f11feeef21
ramibahrouni47 <32247814+ramibahrouni47@users.noreply.github.com>
2017-10-25 21:47:45 +0100

    some little updates

--- PiEasyMission.Data/Migrations/201710252015158_Easy.Designer.cs
+++ PiEasyMission.Data/Migrations/201710252015158_Easy.Designer.cs
@@ -7,13 +7,13 @@ namespace PiEasyMission.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-beta1-60406")]
-    public sealed partial class yass : IMigrationMetadata
+    public sealed partial class Easy : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(yass));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Easy));
         
         string IMigrationMetadata.Id
         {
-            get { return "201710251122493_yass"; }
+            get { return "201710252015158_Easy"; }
         }
         
         string IMigrationMetadata.Source
--- PiEasyMission.Data/Migrations/201710252015158_Easy.cs
+++ PiEasyMission.Data/Migrations/201710252015158_Easy.cs
@@ -3,16 +3,16 @@ namespace PiEasyMission.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class yass : DbMigration
+    public partial class Easy : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.Members", "profilePhoto", c => c.Binary());
+            AddColumn("dbo.Bids", "SkillName", c => c.String());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.Members", "profilePhoto");
+            DropColumn("dbo.Bids", "SkillName");
         }
     }
 }
