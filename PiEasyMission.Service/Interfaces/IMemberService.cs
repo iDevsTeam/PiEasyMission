9f8cf437ec0c7f813caa546c35745d9667ed5f95
yasmine Bennani <beya1994@live.fr>
2017-10-25 14:25:52 +0200

    Add MemberService

---
+++ PiEasyMission.Service/Interfaces/IMemberService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PiEasyMission.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace PiEasyMission.Service.Interfaces
+{
+   public  interface IMemberService : ServicesPattern.IService<Member>
+    {
+        void createMember(Member i);
+        List<Member> getAllMember();
+        void updateMember(Member i);
+        Member getMemberById(int id);
+        void deleteMember(Member i);
+
+        void deleteMemberById(int id);
+    }
+}
---
+++ PiEasyMission.Service/Repositories/MemberService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PiEasyMission.Service.Interfaces;
+using ServicesPattern;
+using PiEasyMission.Data.Infrastructure;
+using PiEasyMission.Domain.Entities;
+
+namespace PiEasyMission.Service.Repositories
+{
+    public class MemberService : Service<Member>, IMemberService
+    {
+        public static IDataBaseFactory dbFactory;
+        public static IUnitOfWork myUnit;
+
+        public MemberService() : base(myUnit)
+        {
+            dbFactory = new DataBaseFactory();
+            myUnit = new UnitOfWork(dbFactory);
+        }
+
+        public void createMember(Member i)
+        {
+            myUnit.getRepository<Member>().Add(i);
+            myUnit.Commit();
+        }
+
+        public void deleteMember(Member i)
+        {
+            myUnit.getRepository<Member>().Delete(i);
+            myUnit.Commit();
+        }
+
+        public void deleteMemberById(int id)
+        {
+            Member i = myUnit.getRepository<Member>().GetById(id);
+            myUnit.getRepository<Member>().Delete(i);
+            myUnit.Commit();
+        }
+
+        public List<Member> getAllMember()
+        {
+            return myUnit.getRepository<Member>().getAll().ToList();
+        }
+
+        public Member getMemberById(int id)
+        {
+            return myUnit.getRepository<Member>().GetById(id);
+        }
+
+
+
+        public void updateMember(Member i)
+        {
+            myUnit.getRepository<Member>().Update(i);
+            myUnit.Commit();
+        }
+    }
+}
